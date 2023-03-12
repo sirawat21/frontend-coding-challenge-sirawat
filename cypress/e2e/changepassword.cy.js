b8aba3aae9e6586306b9c81854cbbc6c53d6e3f3
Brett Sira <sirawat21@gmail.com>
2023-03-12 23:56:18 +1100

    Rewrited test case detail and remove all comments

--- cypress/e2e/changepassword.cy.js
+++ cypress/e2e/changepassword.cy.js
@@ -4,15 +4,13 @@ describe("Change password page E2E test", () => {
     cy.visit("localhost:3000");
   });
 
-  /* Test show an error message when form no input */
   it("Test show an error message when form no input", () => {
     cy.get("button[name*='primary']").click();
     cy.get("p[name*='errorMessageofoldPasswordFormInput']").should('exist');
     cy.get("p[name*='errorMessageofnewPasswordFormInput']").should('exist');
   });
 
-  /* Test show an error message when old password input is no value */
-  it('Testing show an error message when old password input is no value', () => {
+  it("Test show an error message when old-password form is no value", () => {
     cy.get("#oldPasswordFormInput").type(" ");
     cy.get("#newPasswordFormInput").type("abcdd12345");
     cy.get('button[name*="primary"]').click();
