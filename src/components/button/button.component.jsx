2252535c96535768e838ece18f9a2ba4f486da1d
Brett Sira <sirawat21@gmail.com>
2023-03-12 21:13:53 +1100

    Added more CSS style from TailwindUI for button primary

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,16 +1,28 @@
-const Button = ({ children, props }) => {
+const Button = ({ buttonType, children, props }) => {
     const buttonStyle = {
-        default: "rounded-md bg-white py-2 px-3 text-sm font-helvetica text-gray-500 shadow-sm\
-        ring-1 ring-inset ring-gray-300\
-        hover:bg-gray-100\
-        focus-visible:outline\
-        focus-visible:outline-2\
-        focus-visible:outline-offset-2\
-        focus-visible:outline-cyan-500\
+        default: 
+            "rounded-md bg-white py-2 px-3 text-sm font-helvetica text-gray-500 shadow-sm\
+            ring-1 ring-inset ring-gray-300\
+            hover:bg-gray-100\
+            focus-visible:outline\
+            focus-visible:outline-2\
+            focus-visible:outline-offset-2\
+            focus-visible:outline-cyan-500\
+        ",
+        primary: 
+            "rounded-md bg-cyan-600 py-2 px-3 text-sm font-helvetica text-white shadow-sm\
+            hover:bg-cyan-500\
+            focus-visible:outline\
+            focus-visible:outline-2\
+            focus-visible:outline-offset-2\
+            focus-visible:outline-cyan-500\
         ",
     }
     return(
-        <button {...props} className={buttonStyle['default']}>
+        <button  
+            name={buttonType}
+            className={buttonStyle[buttonType]}
+            {...props}>
             {children}
         </button>
     );
