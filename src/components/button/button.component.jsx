c7d28a85f71ec5e67097cfe4c87603e63117c5c1
Brett Sira <sirawat21@gmail.com>
2023-03-12 21:04:57 +1100

    Applied TailwindUI style to Button component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,6 +1,16 @@
 const Button = ({ children, props }) => {
-  return(
-        <button {...props}>
+    const buttonStyle = {
+        default: "rounded-md bg-white py-2 px-3 text-sm font-helvetica text-gray-500 shadow-sm\
+        ring-1 ring-inset ring-gray-300\
+        hover:bg-gray-100\
+        focus-visible:outline\
+        focus-visible:outline-2\
+        focus-visible:outline-offset-2\
+        focus-visible:outline-cyan-500\
+        ",
+    }
+    return(
+        <button {...props} className={buttonStyle['default']}>
             {children}
         </button>
     );
