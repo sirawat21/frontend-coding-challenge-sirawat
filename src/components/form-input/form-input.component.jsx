bef53a197f03f833ec6eebc7eaa60bc6bbbc89f3
Brett Sira <sirawat21@gmail.com>
2023-03-12 21:10:05 +1100

    Applied TailwindUI style to Form Input component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,9 +1,31 @@
 const FormInput = ({ props }) => {
+    const formInputStyle = {
+        label: 
+            "text-md text-gray-900\
+        ",
+        errorMessage:
+            "text-xs\
+        ",
+        form:        
+            "w-full p-1 py-1.5 text-gray-900\
+            border-0 rounded-md shadow-sm ring-1 ring-inset ring-gray-300\
+            hover:ring-gray-400\
+            focus:outline-none\
+            focus:ring-1\
+            focus:ring-inset\
+            focus:ring-orange-600\
+        ",
+        formInvalid:
+            "\
+        ",
+    }
     return(
         <>
-            <input {...props}/>
-            <label>Label</label>
-            <p>ErrorMessage</p>
+            <input 
+            className={formInputStyle["form"]}
+            {...props}/>
+            <label className={formInputStyle["label"]}>Label</label>
+            <p className={formInputStyle["errorMessage"]}>ErrorMessage</p>
         </>
     );
 };
