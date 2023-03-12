cf6b104ed75849a19f8e966e91770210f731b766
Brett Sira <sirawat21@gmail.com>
2023-03-12 21:21:41 +1100

    Group button components into flex box

--- src/App.js
+++ src/App.js
@@ -5,26 +5,33 @@ function App() {
     <div>
       {/* Grid Table; contains 5 rows */}
       <div className="grid grid-row-5 grid-cols-1">
-        {/* [1] Grid Row; Title */}
+        {/* Grid Row[1]; Title */}
         <div className="col-span-1">
           <p className="text-[30px]">Change Password</p>
         </div>
-        {/* [2] Grid Row; Form input old password */}
+        {/* Grid Row[2]; Form input old password */}
         <div className="col-span-1">
           <FormInput placeholder="Old Password" />
         </div>
-        {/* [3] Grid Row; Form input new password  */}
+        {/* Grid Row[3]; Form input new password  */}
         <div className="col-span-1">
           <FormInput placeholder="New Password" />
         </div>
-        {/* [4] Grid Row; Form input confirm password  */}
+        {/* Grid Row[4]; Form input confirm password  */}
         <div className="col-span-1">
           <FormInput placeholder="Confirm Password" />
         </div>
-        {/* [5] Grid Row; Buttons */}
+        {/* Grid Row[5]; Buttons */}
         <div className="col-span-1">
-          <Button buttonType="default">Cancel</Button>
-          <Button buttonType="primary">Change Password</Button>
+          {/* Flex Box; Inline direction */}
+          <div className="flex">
+            <div>
+              <Button buttonType="default">Cancel</Button>
+            </div>
+            <div>
+              <Button buttonType="primary">Change Password</Button>
+            </div>          
+          </div>
         </div>
       </div>
     </div>
