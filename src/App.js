6711b1e07122fb957dc684a97bd3b00b64e452cf
Brett Sira <sirawat21@gmail.com>
2023-03-12 20:53:52 +1100

    Implemented grid table for structuing the layout

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import { Button, FormInput } from "./components";
 function App() {
   return (
     <div>
-      <FormInput placeholder="Old Password" />
-      <FormInput placeholder="New Password" />
-      <FormInput placeholder="Confirm Password" />
-      <Button>
-        Cancel
-      </Button>
-      <Button>
-        Change Password
-      </Button>
+      {/* Grid Table; contains 5 rows */}
+      <div className="grid grid-row-5 grid-cols-1">
+        {/* [1] Grid Row; Title */}
+        <div className="col-span-1">
+          <p className="text-[30px]">Change Password</p>
+        </div>
+        {/* [2] Grid Row; Form input old password */}
+        <div className="col-span-1">
+          <FormInput placeholder="Old Password" />
+        </div>
+        {/* [3] Grid Row; Form input new password  */}
+        <div className="col-span-1">
+          <FormInput placeholder="New Password" />
+        </div>
+        {/* [4] Grid Row; Form input confirm password  */}
+        <div className="col-span-1">
+          <FormInput placeholder="Confirm Password" />
+        </div>
+        {/* [5] Grid Row; Buttons */}
+        <div className="col-span-1">
+          <Button>Cancel</Button>
+          <Button>Change Password</Button>
+        </div>
+      </div>
     </div>
   );
 }
