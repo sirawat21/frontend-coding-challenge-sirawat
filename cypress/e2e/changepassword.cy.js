b3191e9c85cf9fcf8e4c91850f488e0fd5cda8cb
Brett Sira <sirawat21@gmail.com>
2023-03-12 23:49:58 +1100

    Added test case show error when click submit without any inputs

--- cypress/e2e/changepassword.cy.js
+++ cypress/e2e/changepassword.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/* Change password page E2E test */
+describe("Change password page E2E test", () => {
+  beforeEach(() => {
+    cy.visit("localhost:3000");
+  });
+
+  /* Test show an error message when form no input */
+  it("Test show an error message when form no input", () => {
+    cy.get("button[name*='primary']").click();
+    cy.get("p[name*='errorMessageofoldPasswordFormInput']").should('exist');
+    cy.get("p[name*='errorMessageofnewPasswordFormInput']").should('exist');
+  });
+
+});
